91a99627a570a14aabdf37ec3253db75ba1cc725
Gregor Hermens <gregor.hermens@a-mazing.de>
2026-06-15 15:25:40 +0200

    [FEATURE] PSR14-Event for post-processing the suggestions list

--- Classes/Controller/AutocompleteController.php
+++ Classes/Controller/AutocompleteController.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace RKL\AutocompleteForIndexedSearch\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use RKL\AutocompleteForIndexedSearch\Event\PostProcessSuggestionsEvent;
 use RKL\AutocompleteForIndexedSearch\Service\SuggestionsService;
 use RKL\AutocompleteForIndexedSearch\Utility\SearchWordsArrayUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
@@ -56,6 +57,11 @@ final class AutocompleteController extends ActionController
 		} else {
 			$suggestions = [];
 		}
+
+		$event = $this->eventDispatcher->dispatch(
+			new PostProcessSuggestionsEvent($suggestions, $this->settings)
+		);
+		$suggestions = $event->getSuggestions();
 
 		$this->view->assign('suggestions', $suggestions);
 
---
+++ Classes/Event/PostProcessSuggestionsEvent.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RKL\AutocompleteForIndexedSearch\Event;
+
+final class PostProcessSuggestionsEvent {
+	public function __construct(
+		private array $suggestions,
+		private readonly array $settings,
+	) {}
+
+	public function getSuggestions(): array
+	{
+		return $this->suggestions;
+	}
+
+	public function setSuggestions(array $suggestions): void
+	{
+		$this->suggestions = $suggestions;
+	}
+
+	public function getSettings(): array
+	{
+		return $this->settings;
+	}
+}
